7cbbd3846eb7ed2f197bf6a9a3954ba2d0c27939
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-04-26 15:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function nowServing(name) {
   if (katzDeliLine.length===0) {
     return "There is nobody waiting to be served!"
   }
- else 
- return 
+ else {
+ return `Currently serving ${name}.`
+ katzDeliline.shift(name)
     }
   }
