d619648249313cd6a99a2ecbd6539dfccea19fe4
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-04-26 16:34:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function currentLine(line){
   if (line.length ===0) {
     return "The line is currently empty."
   }
-  var fullLine = []
+  const fullLine = []
   for (let i = 0, l = line.length; i < l; i++) {
     
     fullLine.push(`${i+1}. ${line[i]}`)
