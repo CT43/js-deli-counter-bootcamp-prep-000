7fca57f4200a02f6ff4be5f2500e605d338637fa
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-04-26 15:38:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var katzDeli = []
+var katzDeliLine = []
 
 function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name)
   return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.'
 }
+function nowServing(name) {
+  if (line.length>0) {
+    return name
+    katzDeliLine.shift(name)}
+    else ()
+    return "There is nobody waiting to be served!"
+    
+  }
